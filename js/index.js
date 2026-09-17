419aaea6f222cf5456cb4aea6ac2da024ae885f7
leandramorenoo <morenoo.leandra@alumnos.cei.es>
2026-09-17 11:05:39 +0200

    cambio color header

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
-// MENU DESPLEGABLE
+
 
 const menu = document.getElementById('menu')
 const toggleMenuIcon = document.getElementById('toggleMenuIcon')
+const header = document.querySelector('.header')
 
-function toggleMenu() {
-
+// MENU DESPLEGABLE
+toggleMenuIcon.addEventListener('click', e => {
     menu.classList.toggle('hidden')
     toggleMenuIcon.classList.toggle('white')
+})
 
-}
+// CAMBIAR COLOR HEADER
+
+document.addEventListener('scroll', e => {
+    console.log(window.scrollY)
+        if(window.scrollY > 320) {
+            header.classList.add('sticky')
+        } else {
+            header.classList.remove('sticky')
+        }
+})
+   
+
